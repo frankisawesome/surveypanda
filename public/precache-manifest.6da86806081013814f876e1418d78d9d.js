f57c24ab3aeb9544dd393cac9e802f1ad42aa483
Frank <b439988l@gmail.com>
2019-10-26 15:23:26 +1000

    react page

---
+++ public/precache-manifest.6da86806081013814f876e1418d78d9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3bcf566e98ad1470f15fc167c4b25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "225af0d483d2d1eb2dfd",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fc3e315b414e0a833f98",
+    "url": "/static/css/main.d9f598b3.chunk.css"
+  },
+  {
+    "revision": "225af0d483d2d1eb2dfd",
+    "url": "/static/js/2.f995a26d.chunk.js"
+  },
+  {
+    "revision": "fc3e315b414e0a833f98",
+    "url": "/static/js/main.1cf51566.chunk.js"
+  },
+  {
+    "revision": "965b05a6a05f2b445c7e",
+    "url": "/static/js/runtime-main.d04efc3a.js"
+  },
+  {
+    "revision": "fc9ed023d810750f8d6c8dd0b761737e",
+    "url": "/static/media/smallpanda.fc9ed023.jpg"
+  }
+]);
