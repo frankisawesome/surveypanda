6a0ad3fca1b47b8f6cd8e6bd6f6748f8e09a7e91
Frank <b439988l@gmail.com>
2019-10-26 17:56:32 +1000

    final final

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.99d0d5ecba042607e38730bcb5b2fb35.js"
+  "/precache-manifest.7551f36680eebb3522f902662fc41671.js"
 );
 
 self.addEventListener('message', (event) => {
