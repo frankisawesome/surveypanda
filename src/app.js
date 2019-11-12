ef95961b24465b1f9ec491e2d268a19e67f9db8a
Frank <b439988l@gmail.com>
2019-11-12 10:10:18 +1000

    general cleanup

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//Packages import
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -5,33 +6,38 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require("cors")
+const dotenv = require("dotenv")
 
+//Endpoint routers
 var surveysRouter = require('./api/surveys');
 var companyRouter = require('./api/companyApi');
 
+//App
 var app = express();
 
-require('dotenv').config();
-const cors = require("cors")
+//essentials
+app.use(helmet())
 app.use(cors())
-
-//db connection
-mongoose.connect(process.env.DB_CON)
-  .then(() => console.log('Connection to DB successful'))
-  .catch((err) => console.error(err));
-
-
-
+dotenv.config();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//db connection
+mongoose.connect(process.env.DB_CON)
+  .then(() => console.log('Connection to DB successful'))
+  .catch((err) => console.log(err))
+
+//front end
+app.use(express.static('../reactpanda/build'));
+
+//api routes
 app.use('/surveys', surveysRouter);
 app.use('/company', companyRouter);
 
-// catch 404 and forward to error handler
+//catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
