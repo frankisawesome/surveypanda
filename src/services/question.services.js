dd5c6f14b5d713dc5c64b39d489837c79be7b648
Frank <b439988l@gmail.com>
2019-11-21 17:19:55 +1000

    document returned correctly on creating new questionnare using the /today route

--- src/services/question.services.js
+++ src/services/question.services.js
@@ -3,41 +3,45 @@ const companyServices = require('../services/company.services')
 
 module.exports = {
     create,
-    find
+    find,
+    updateAnswers
 }
 
 //find a questionnare for a particular company on a given day
-async function find(name, date){
-    const upper = new Date(date.getFullYear(), date.getMonth(), date.getDate()+1)
+function find(name, date) {
+    const upper = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1)
     const lower = new Date(date.getFullYear(), date.getMonth(), date.getDate())
 
-    const query = QuestionSet.find({ companyName: name, date: { $lte: upper, $gte: lower}})
+    const query = QuestionSet.find({ companyName: name, date: { $lte: upper, $gte: lower } })
 
     return query.exec()
 }
 
-async function create(name){
+async function create(name) {
     //find the company document by name
-    let company;
-    companyServices.find(name)
-    .then((doc) => {
+    try {
+        let companyArr = await companyServices.find(name)
+        const company = companyArr[0]
         const newSet = new QuestionSet({
             companyName: name,
             questions: []
         })
-        company = doc[0]
         company.questions.map((question) => {
             const newq = {
                 text: question.text,
                 measures: question.measures,
                 results: []
             }
-            
+
             newSet.questions.push(newq)
         })
         return newSet.save()
-    })
-    .catch((err) => {
+    }
+    catch (err) {
         throw err;
-    })
+    }
+}
+
+async function updateAnswers() {
+
 }
