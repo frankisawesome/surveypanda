010d7a6a9a396810a854eb0141558cbdec56f735
Frank <b439988l@gmail.com>
2019-12-02 11:49:37 +1000

    computation moved to search.services

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -1,8 +1,7 @@
-var Router = require('express');
-var router = Router();
-const questionServices = require('../services/question.services');
-const searchServices = require('../services/search.services');
-const arrayEqual = require('array-equal')
+var Router = require('express')
+var router = Router()
+const questionServices = require('../services/question.services')
+const searchServices = require('../services/search.services')
 
 //API Endpoints
 router.get('/day', getDaily)
@@ -50,49 +49,7 @@ async function getWeekly(req, res) {
     try {
         let qsetArr = await questionServices.findWeek(name, date)
 
-        if (qsetArr.length === 0) {
-            throw {
-                error: true,
-                message: "Can't find requested set of questionnaires, could be: 1. the questionnaire does not exist 2. request body error, check that you have a correct name field in your request body, and a correct week value"
-            }
-        }
-
-        const numOfSet = qsetArr.length
-        const setOfSummary = []
-        let resultSummary
-        const averageSums = []
-
-        qsetArr.map((qset) => {
-            setOfSummary.push(qset.summary)
-        })
-
-        setOfSummary.map((sum, i) => {
-            if (i == 0) {
-                resultSummary = {
-                    measures: sum.measures,
-                    averages: []
-                }
-                sum.averages.map((average) => {
-                    averageSums.push(average)
-                })
-            }
-            else {
-                if (!arrayEqual(sum.measures, resultSummary.measures)) {
-                    throw {
-                        error: true,
-                        message: "Changes have been made to your measuring metrics in the past week, try again next week!"
-                    }
-                }
-                else {
-                    sum.averages.map((average, j) => {
-                        averageSums[j] += average
-                    })
-                }
-            }
-        })
-        for (i = 0; i < averageSums.length; i++) {
-            resultSummary.averages[i] = averageSums[i] / numOfSet
-        }
+        const resultSummary = searchServices.summaryForArray(qsetArr)
 
         res.status(200)
         res.send(resultSummary)
--- src/services/search.services.js
+++ src/services/search.services.js
@@ -1,7 +1,9 @@
 const QuestionSet = require('../models/questionSet')
+const arrayEqual = require('array-equal')
 
 module.exports = {
-    summary
+    summary,
+    summaryForArray
 }
 
 //returns a summary statistics object of a given questionSet object
@@ -22,5 +24,53 @@ function summary(qset) {
         summary.measures.push(question.measures)
     })
     return summary
+}
+
+function summaryForArray(qsetArr) {
+    if (qsetArr.length === 0) {
+        throw {
+            error: true,
+            message: "Can't find requested set of questionnaires, could be: 1. the questionnaire does not exist 2. request body error, check that you have a correct name field in your request body, and a correct week value"
+        }
+    }
+
+    const numOfSet = qsetArr.length
+    const setOfSummary = []
+    let resultSummary
+    const averageSums = []
+
+    qsetArr.map((qset) => {
+        setOfSummary.push(qset.summary)
+    })
+
+    setOfSummary.map((sum, i) => {
+        if (i == 0) {
+            resultSummary = {
+                measures: sum.measures,
+                averages: []
+            }
+            sum.averages.map((average) => {
+                averageSums.push(average)
+            })
+        }
+        else {
+            if (!arrayEqual(sum.measures, resultSummary.measures)) {
+                throw {
+                    error: true,
+                    message: "Changes have been made to your measuring metrics in the past week, try again next week!"
+                }
+            }
+            else {
+                sum.averages.map((average, j) => {
+                    averageSums[j] += average
+                })
+            }
+        }
+    })
+    for (i = 0; i < averageSums.length; i++) {
+        resultSummary.averages[i] = averageSums[i] / numOfSet
+    }
+
+    return resultSummary
 }
 
